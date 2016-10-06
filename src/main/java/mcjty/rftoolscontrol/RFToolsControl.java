da2196799379430ea2c14a4d7c77939f33ed8a3d
McJty <mcjty1@gmail.com>
2016-10-06 21:07:27 +0200

    Update to McJtyLib 2.1.4

--- src/main/java/mcjty/rftoolscontrol/RFToolsControl.java
+++ src/main/java/mcjty/rftoolscontrol/RFToolsControl.java
@@ -29,7 +29,7 @@ public class RFToolsControl implements ModBase {
     public static final String MODID = "rftoolscontrol";
     public static final String VERSION = "1.3.2";
     public static final String MIN_FORGE_VER = "12.16.1.1896";
-    public static final String MIN_MCJTYLIB_VER = "1.10-2.1.1";
+    public static final String MIN_MCJTYLIB_VER = "1.10-2.1.4";
     public static final String MIN_RFTOOLS_VER = "1.10-5.22";
 
     @SidedProxy(clientSide="mcjty.rftoolscontrol.proxy.ClientProxy", serverSide="mcjty.rftoolscontrol.proxy.ServerProxy")
