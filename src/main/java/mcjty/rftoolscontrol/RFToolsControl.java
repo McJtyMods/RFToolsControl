52268ecc7961ed14ecf42741e5574a3887dcc1fc
McJty <mcjty1@gmail.com>
2016-11-26 06:32:18 +0100

    1.5.2alpha1

--- src/main/java/mcjty/rftoolscontrol/RFToolsControl.java
+++ src/main/java/mcjty/rftoolscontrol/RFToolsControl.java
@@ -25,7 +25,8 @@ import net.minecraftforge.fml.common.event.*;
 //        "required-after:Forge@["+ RFToolsControl.MIN_FORGE_VER+",);" +
 //                "required-after:rftools@["+RFToolsControl.MIN_RFTOOLS_VER+",);"+
 //                        "required-after:McJtyLib@["+ RFToolsControl.MIN_MCJTYLIB_VER+",)",
-        version = RFToolsControl.VERSION)
+        version = RFToolsControl.VERSION,
+        acceptedMinecraftVersions = "[1.10,1.12)")
 public class RFToolsControl implements ModBase {
     public static final String MODID = "rftoolscontrol";
     public static final String VERSION = "1.5.2alpha1";
