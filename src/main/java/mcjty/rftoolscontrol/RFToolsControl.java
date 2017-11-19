27e90c36c4cce6f3e9de916b60ee0b3d02e53792
McJty <mcjty1@gmail.com>
2017-11-19 08:21:38 +0100

    1.6.9

--- src/main/java/mcjty/rftoolscontrol/RFToolsControl.java
+++ src/main/java/mcjty/rftoolscontrol/RFToolsControl.java
@@ -32,7 +32,7 @@ import net.minecraftforge.fml.common.event.*;
         acceptedMinecraftVersions = "[1.10,1.12)")
 public class RFToolsControl implements ModBase {
     public static final String MODID = "rftoolscontrol";
-    public static final String VERSION = "1.6.8";
+    public static final String VERSION = "1.6.9";
     public static final String MIN_RFTOOLS_VER = "7.14";
     public static final String MIN_FORGE10_VER = "12.18.1.2082";
     public static final String MIN_FORGE11_VER = "13.19.0.2176";
