f8a5dfe3cca10a048cf66a464320af6db5fe2837
McJty <mcjty1@gmail.com>
2017-12-23 05:53:44 +0100

    Bump

--- src/main/java/mcjty/rftoolscontrol/RFToolsControl.java
+++ src/main/java/mcjty/rftoolscontrol/RFToolsControl.java
@@ -30,10 +30,10 @@ import java.util.function.Function;
         version = RFToolsControl.VERSION)
 public class RFToolsControl implements ModBase {
     public static final String MODID = "rftoolscontrol";
-    public static final String VERSION = "1.7.0";
-    public static final String MIN_RFTOOLS_VER = "7.17";
+    public static final String VERSION = "1.7.1";
+    public static final String MIN_RFTOOLS_VER = "7.18";
     public static final String MIN_FORGE11_VER = "13.19.0.2176";
-    public static final String MIN_MCJTYLIB_VER = "2.5.2";
+    public static final String MIN_MCJTYLIB_VER = "2.5.3";
 
     @SidedProxy(clientSide="mcjty.rftoolscontrol.proxy.ClientProxy", serverSide="mcjty.rftoolscontrol.proxy.ServerProxy")
     public static CommonProxy proxy;
