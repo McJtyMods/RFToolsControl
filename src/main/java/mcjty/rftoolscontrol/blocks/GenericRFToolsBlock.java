606f986360d766e2556afe68b471dbb1f41d0245
McJty <mcjty1@gmail.com>
2018-05-19 20:43:02 +0200

    Various fixes

--- src/main/java/mcjty/rftoolscontrol/blocks/GenericRFToolsBlock.java
+++ src/main/java/mcjty/rftoolscontrol/blocks/GenericRFToolsBlock.java
@@ -3,6 +3,7 @@ package mcjty.rftoolscontrol.blocks;
 import mcjty.lib.blocks.GenericBlock;
 import mcjty.lib.tileentity.GenericTileEntity;
 import mcjty.rftoolscontrol.RFToolsControl;
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
@@ -12,6 +13,7 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public abstract class GenericRFToolsBlock<T extends GenericTileEntity, C extends Container> extends GenericBlock<T, C> {
 
@@ -20,8 +22,9 @@ public abstract class GenericRFToolsBlock<T extends GenericTileEntity, C extends
         setCreativeTab(RFToolsControl.tabRFToolsControl);
     }
 
-    public GenericRFToolsBlock(Material material, Class<? extends T> tileEntityClass, BiFunction<EntityPlayer, IInventory, C> containerFactory, Class<? extends ItemBlock> itemBlockClass, String name, boolean isContainer) {
-        super(RFToolsControl.instance, material, tileEntityClass, containerFactory, itemBlockClass, name, isContainer);
+    public GenericRFToolsBlock(Material material, Class<? extends T> tileEntityClass, BiFunction<EntityPlayer, IInventory, C> containerFactory,
+                               Function<Block, ItemBlock> itemBlockFunction, String name, boolean isContainer) {
+        super(RFToolsControl.instance, material, tileEntityClass, containerFactory, itemBlockFunction, name, isContainer);
         setCreativeTab(RFToolsControl.tabRFToolsControl);
     }
 
