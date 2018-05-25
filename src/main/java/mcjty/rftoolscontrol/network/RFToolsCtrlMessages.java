ba836ee46499f3c79f8a5535737964ec2f953528
McJty <mcjty1@gmail.com>
2018-05-25 12:22:19 +0200

    Various fixes

--- src/main/java/mcjty/rftoolscontrol/network/RFToolsCtrlMessages.java
+++ src/main/java/mcjty/rftoolscontrol/network/RFToolsCtrlMessages.java
@@ -14,26 +14,26 @@ public class RFToolsCtrlMessages {
         INSTANCE = net;
 
         // Server side
-        net.registerMessage(PacketGetLog.Handler.class, PacketGetLog.class, PacketHandler.nextID(), Side.SERVER);
-        net.registerMessage(PacketGetDebugLog.Handler.class, PacketGetDebugLog.class, PacketHandler.nextID(), Side.SERVER);
-        net.registerMessage(PacketGetVariables.Handler.class, PacketGetVariables.class, PacketHandler.nextID(), Side.SERVER);
-        net.registerMessage(PacketGetFluids.Handler.class, PacketGetFluids.class, PacketHandler.nextID(), Side.SERVER);
-        net.registerMessage(PacketGetTankFluids.Handler.class, PacketGetTankFluids.class, PacketHandler.nextID(), Side.SERVER);
-        net.registerMessage(PacketGetCraftableItems.Handler.class, PacketGetCraftableItems.class, PacketHandler.nextID(), Side.SERVER);
-        net.registerMessage(PacketGetRequests.Handler.class, PacketGetRequests.class, PacketHandler.nextID(), Side.SERVER);
-        net.registerMessage(PacketSendRecipe.Handler.class, PacketSendRecipe.class, PacketHandler.nextID(), Side.SERVER);
-        net.registerMessage(PacketItemNBTToServer.Handler.class, PacketItemNBTToServer.class, PacketHandler.nextID(), Side.SERVER);
-        net.registerMessage(PacketVariableToServer.Handler.class, PacketVariableToServer.class, PacketHandler.nextID(), Side.SERVER);
-        net.registerMessage(PacketUpdateNBTItemInventoryProgrammer.Handler.class, PacketUpdateNBTItemInventoryProgrammer.class, PacketHandler.nextID(), Side.SERVER);
-        net.registerMessage(PacketUpdateNBTItemCard.Handler.class, PacketUpdateNBTItemCard.class, PacketHandler.nextID(), Side.SERVER);
+        net.registerMessage(PacketGetLog.Handler.class, PacketGetLog.class, PacketHandler.nextPacketID(), Side.SERVER);
+        net.registerMessage(PacketGetDebugLog.Handler.class, PacketGetDebugLog.class, PacketHandler.nextPacketID(), Side.SERVER);
+        net.registerMessage(PacketGetVariables.Handler.class, PacketGetVariables.class, PacketHandler.nextPacketID(), Side.SERVER);
+        net.registerMessage(PacketGetFluids.Handler.class, PacketGetFluids.class, PacketHandler.nextPacketID(), Side.SERVER);
+        net.registerMessage(PacketGetTankFluids.Handler.class, PacketGetTankFluids.class, PacketHandler.nextPacketID(), Side.SERVER);
+        net.registerMessage(PacketGetCraftableItems.Handler.class, PacketGetCraftableItems.class, PacketHandler.nextPacketID(), Side.SERVER);
+        net.registerMessage(PacketGetRequests.Handler.class, PacketGetRequests.class, PacketHandler.nextPacketID(), Side.SERVER);
+        net.registerMessage(PacketSendRecipe.Handler.class, PacketSendRecipe.class, PacketHandler.nextPacketID(), Side.SERVER);
+        net.registerMessage(PacketItemNBTToServer.Handler.class, PacketItemNBTToServer.class, PacketHandler.nextPacketID(), Side.SERVER);
+        net.registerMessage(PacketVariableToServer.Handler.class, PacketVariableToServer.class, PacketHandler.nextPacketID(), Side.SERVER);
+        net.registerMessage(PacketUpdateNBTItemInventoryProgrammer.Handler.class, PacketUpdateNBTItemInventoryProgrammer.class, PacketHandler.nextPacketID(), Side.SERVER);
+        net.registerMessage(PacketUpdateNBTItemCard.Handler.class, PacketUpdateNBTItemCard.class, PacketHandler.nextPacketID(), Side.SERVER);
 
         // Client side
-        net.registerMessage(PacketLogReady.Handler.class, PacketLogReady.class, PacketHandler.nextID(), Side.CLIENT);
-        net.registerMessage(PacketVariablesReady.Handler.class, PacketVariablesReady.class, PacketHandler.nextID(), Side.CLIENT);
-        net.registerMessage(PacketFluidsReady.Handler.class, PacketFluidsReady.class, PacketHandler.nextID(), Side.CLIENT);
-        net.registerMessage(PacketTankFluidsReady.Handler.class, PacketTankFluidsReady.class, PacketHandler.nextID(), Side.CLIENT);
-        net.registerMessage(PacketCraftableItemsReady.Handler.class, PacketCraftableItemsReady.class, PacketHandler.nextID(), Side.CLIENT);
-        net.registerMessage(PacketRequestsReady.Handler.class, PacketRequestsReady.class, PacketHandler.nextID(), Side.CLIENT);
-        net.registerMessage(PacketGraphicsReady.Handler.class, PacketGraphicsReady.class, PacketHandler.nextID(), Side.CLIENT);
+        net.registerMessage(PacketLogReady.Handler.class, PacketLogReady.class, PacketHandler.nextPacketID(), Side.CLIENT);
+        net.registerMessage(PacketVariablesReady.Handler.class, PacketVariablesReady.class, PacketHandler.nextPacketID(), Side.CLIENT);
+        net.registerMessage(PacketFluidsReady.Handler.class, PacketFluidsReady.class, PacketHandler.nextPacketID(), Side.CLIENT);
+        net.registerMessage(PacketTankFluidsReady.Handler.class, PacketTankFluidsReady.class, PacketHandler.nextPacketID(), Side.CLIENT);
+        net.registerMessage(PacketCraftableItemsReady.Handler.class, PacketCraftableItemsReady.class, PacketHandler.nextPacketID(), Side.CLIENT);
+        net.registerMessage(PacketRequestsReady.Handler.class, PacketRequestsReady.class, PacketHandler.nextPacketID(), Side.CLIENT);
+        net.registerMessage(PacketGraphicsReady.Handler.class, PacketGraphicsReady.class, PacketHandler.nextPacketID(), Side.CLIENT);
     }
 }
