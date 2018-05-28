beb9308ce92d3405851332379623d283c434d3c7
romelo333 <romelo333@gmail.com>
2018-05-28 20:44:39 +0200

    More project updates

--- src/main/java/mcjty/rftoolscontrol/RFToolsControl.java
+++ src/main/java/mcjty/rftoolscontrol/RFToolsControl.java
@@ -30,10 +30,10 @@ import java.util.function.Function;
         version = RFToolsControl.VERSION)
 public class RFToolsControl implements ModBase {
     public static final String MODID = "rftoolscontrol";
-    public static final String VERSION = "1.8.1";
-    public static final String MIN_RFTOOLS_VER = "7.23";
+    public static final String VERSION = "1.9.0alpha";
+    public static final String MIN_RFTOOLS_VER = "7.50";
     public static final String MIN_FORGE11_VER = "13.19.0.2176";
-    public static final String MIN_MCJTYLIB_VER = "2.6.2";
+    public static final String MIN_MCJTYLIB_VER = "3.0.0";
 
     @SidedProxy(clientSide="mcjty.rftoolscontrol.proxy.ClientProxy", serverSide="mcjty.rftoolscontrol.proxy.ServerProxy")
     public static CommonProxy proxy;
