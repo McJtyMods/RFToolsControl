154287686d8b97817f03295135e6a3ee376b8a43
McJty <mcjty1@gmail.com>
2018-11-16 05:53:35 +0100

    1.9.2

--- src/main/java/mcjty/rftoolscontrol/RFToolsControl.java
+++ src/main/java/mcjty/rftoolscontrol/RFToolsControl.java
@@ -30,7 +30,7 @@ import java.util.function.Function;
         version = RFToolsControl.VERSION)
 public class RFToolsControl implements ModBase {
     public static final String MODID = "rftoolscontrol";
-    public static final String VERSION = "1.9.1";
+    public static final String VERSION = "1.9.2";
     public static final String MIN_RFTOOLS_VER = "7.58";
     public static final String MIN_FORGE11_VER = "13.19.0.2176";
     public static final String MIN_MCJTYLIB_VER = "3.0.5";
