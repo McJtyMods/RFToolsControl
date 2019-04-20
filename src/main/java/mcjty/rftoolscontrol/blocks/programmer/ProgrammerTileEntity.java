d52f1bea4f1b614087e5cfa4ef63d4b7e67e9f89
McJty <mcjty1@gmail.com>
2019-04-20 07:26:06 +0200

    If the programmer gets a redstone signal it will save the current program to the card. This allows automation to save a program to many cards

--- src/main/java/mcjty/rftoolscontrol/blocks/programmer/ProgrammerTileEntity.java
+++ src/main/java/mcjty/rftoolscontrol/blocks/programmer/ProgrammerTileEntity.java
@@ -4,8 +4,11 @@ import mcjty.lib.container.DefaultSidedInventory;
 import mcjty.lib.container.InventoryHelper;
 import mcjty.lib.tileentity.GenericTileEntity;
 import mcjty.rftoolscontrol.items.ModItems;
+import mcjty.rftoolscontrol.items.ProgramCardItem;
+import mcjty.rftoolscontrol.logic.grid.ProgramCardInstance;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
 
 public class ProgrammerTileEntity extends GenericTileEntity implements DefaultSidedInventory {
@@ -14,6 +17,33 @@ public class ProgrammerTileEntity extends GenericTileEntity implements DefaultSi
 
     public ProgrammerTileEntity() {
         inventoryHelper.setStackInSlot(ProgrammerContainer.SLOT_DUMMY, new ItemStack(ModItems.programCardItem));
+    }
+
+    @Override
+    public void setPowerInput(int powered) {
+        int p = powerLevel;
+        super.setPowerInput(powered);
+        if (p != powerLevel && powerLevel > 0) {
+            // Copy program in programmer to card
+            ItemStack dummy = getStackInSlot(ProgrammerContainer.SLOT_DUMMY);
+            if (dummy.isEmpty()) {
+                return;
+            }
+            if (!dummy.hasTagCompound()) {
+                return;
+            }
+            ItemStack card = getStackInSlot(ProgrammerContainer.SLOT_CARD);
+            if (card.isEmpty()) {
+                return;
+            }
+            ProgramCardInstance instance = ProgramCardInstance.parseInstance(dummy);
+            if (instance == null) {
+                return;
+            }
+            ProgramCardItem.setCardName(card, ProgramCardItem.getCardName(dummy));
+            NBTBase newGrid = dummy.getTagCompound().getTag("grid").copy();
+            card.getTagCompound().setTag("grid", newGrid);
+        }
     }
 
     @Override
