b62ba4d9b10dadb489254dc7ebaebba3e1a435d4
McJty <mcjty1@gmail.com>
2019-04-24 18:48:59 +0200

    There was a chance that a programmer would not correctly save its current program on exit. Loading the world again would revert to an older program. This has now been fixed

--- src/main/java/mcjty/rftoolscontrol/blocks/programmer/PacketUpdateNBTItemInventoryProgrammer.java
+++ src/main/java/mcjty/rftoolscontrol/blocks/programmer/PacketUpdateNBTItemInventoryProgrammer.java
@@ -64,6 +64,7 @@ public class PacketUpdateNBTItemInventoryProgrammer implements IMessage {
                 if (!stack.isEmpty()) {
                     stack.setTagCompound(tagCompound);
                 }
+                te.markDirty();
             }
         });
         ctx.setPacketHandled(true);
