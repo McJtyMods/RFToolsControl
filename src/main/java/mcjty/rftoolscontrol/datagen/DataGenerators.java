82c787af9c7613873fc4de23c65aa8acd0efa4b6
McJty <mcjty1@gmail.com>
2021-02-19 08:01:43 +0100

    Specify the MODID with all Mod.EventBusSubscriber

--- src/main/java/mcjty/rftoolscontrol/datagen/DataGenerators.java
+++ src/main/java/mcjty/rftoolscontrol/datagen/DataGenerators.java
@@ -1,11 +1,12 @@
 package mcjty.rftoolscontrol.datagen;
 
+import mcjty.rftoolscontrol.RFToolsControl;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+@Mod.EventBusSubscriber(modid = RFToolsControl.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DataGenerators {
 
     @SubscribeEvent
