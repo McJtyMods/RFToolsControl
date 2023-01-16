82dba55f55ce25818852cdbf156e799399a38707
McJty <mcjty1@gmail.com>
2023-01-16 06:35:42 +0100

    Various fixes for 1.19.2/1.19.3 compatibility

--- src/main/java/mcjty/rftoolscontrol/RFToolsControl.java
+++ src/main/java/mcjty/rftoolscontrol/RFToolsControl.java
@@ -52,8 +52,7 @@ public class RFToolsControl {
     }
 
     public static <T extends Item> Supplier<T> tab(Supplier<T> supplier) {
-        instance.setup.tab(supplier);
-        return supplier;
+        return instance.setup.tab(supplier);
     }
 
     private void onDataGen(GatherDataEvent event) {
